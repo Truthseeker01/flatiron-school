9a238c51c7a8291e6b34d54a2e44bbc1627207f6
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-03-14 14:44:51 -0400

    add changes

---
+++ phase-1/events/introduction/beginning/index.js
@@ -0,0 +1,36 @@
+/*
+
+Phase 1 -> DOM Events
+by Sakib Rasul
+Updated March 13, 2024
+Created September 13, 2023
+
+Objectives
+1. Do something when an event fires on an element.
+2. Handle a form submission by reading its input.
+
+Takeaways
+- "click" and "submit" events
+- `event.preventDefault()`
+- `[form].[input].value`
+
+*/
+
+// ~ the "click" event
+// the target(what element the even will fire on), the event(the user action, "click"), the handler/listener(log)
+document.getElementById("today").addEventListener("click", event => {
+    console.log("Today was clicked")
+});
+// ~ the "submit" event
+document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault();
+    console.log("Submitted!");
+    console.log(document.querySelector("form").date);
+    const submittedDate = document.querySelector("form").date.value;
+    console.log(submittedDate);
+})
+// ~ challenges
+// 1. Add some non-click, non-submit event listener to the page. It can trigger whatever you wish!
+document.body.addEventListener("keydown", e => {
+    console.log("Keydown was pressed")
+});
---
+++ phase-1/manipulation/beginning/index.js
@@ -0,0 +1,83 @@
+/*
+
+Phase 1 -> DOM Manipulation
+by Sakib Rasul
+Updated March 13, 2024
+Created September 12, 2023
+
+Core Deliverables
+1. Select a node.
+2. Modify a node.
+3. Remove a node.
+4. Append a node.
+
+Challenges
+1. Append a list.
+2. Replace a node.
+
+*/
+
+// ~ APIs, CRUD, `document`
+
+// ~ Read/Select a node
+// -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
+console.log(document.querySelector("h1"));
+console.log(document.querySelector(".address")); // . before a class name
+console.log(document.querySelector("#today")); // # before an id name
+// -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
+console.log(document.querySelector("p")); // selects the first p is finds
+console.log(document.querySelectorAll("p")); // selects all ps
+document.querySelectorAll("p").forEach(pElement => {
+    console.log(pElement);
+});
+// -> textContent is a property of text nodes (e.g. h1, p) that contain their text.
+console.log(document.querySelector("#today").textContent);
+
+// ~ Update/Modify a node's attributes
+// -> To modify an attribute, just use = after the attribute name in object dot notation
+document.querySelector("#today").textContent = "Today!"; // changes period . to ! 
+document.querySelector("#today").style.color = "red";
+// ~ Delete/Remove a node
+// -> To remove an existing element, we can look it up and call the node's method `remove()`.
+document.querySelector("#tomorrow").remove();
+// ~ Create + Append a node
+// -> createElement(), append()
+const image = document.createElement("img"); // it's not add to html. To add it you need to append it
+image.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcats.com%2Ffemale-cats-facts&psig=AOvVaw3BbksU7Le-nhfTzLZTcj6t&ust=1710440778893000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCND6tc7u8YQDFQAAAAAdAAAAABAE";
+//document.body.append(image);
+document.querySelector("#image-container").append(image); //appending to a specific element
+// ~ Challenges
+// 1. Write a function named displayList that takes a name and an array,
+//    and appends a list to #dates. For example, given "Books" and "The Shining",
+//    the function should append to #dates something like:
+//        Books
+//        • The Shining
+function diplayList(h, arr){
+    const heading = document.createElement("h1");
+    heading.textContent = h;
+    document.querySelector("#dates").append(heading);
+    
+    const list = document.createElement("ul");
+    for (i=0; i < arr.length; i++){
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(arr[i]));
+        list.appendChild(item);
+    };
+
+    arr.forEach(i => {
+        const item = document.createElement("li");
+        item.appendChild(document.createTextNode(i));
+        list.appendChild(item);
+    })
+
+    document.querySelector("#dates").append(list)
+};
+diplayList("Books", ["The shining", "The shining2", "hghh"]);
+// 2. Replace the <strong> element with a newly created one.
+document.querySelector("strong").remove();
+const image2 = document.createElement("img");
+image2.src = "#"
+document.querySelector("address").append(image2);
+
+
+
